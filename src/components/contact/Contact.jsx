97a204c66dd334ac7c9e4e9bc7d6f5abd8a89236
Angelo <angelodeoliveiramix@gmail.com>
2022-12-18 14:48:34 -0400

    Finished the contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,8 +2,30 @@ import './Contact.scss';
 import Phone from '../../assets/phone.png'
 import Email from '../../assets/email.png'
 import Address from '../../assets/address.png'
+import { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser'
+
 
 const Contact = () => {
+
+  const formRef = useRef();
+  const [send,setSend] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_e9sfac9', 'template_3fuucj9', formRef.current, 'b7c_0pqoX-zqK7K6I')
+      .then((result) => {
+          console.log(result.text);
+          setSend(true);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+  }
+
+
+
   return (
     <div className='contact-container'>
         <div className="contact-bg"></div>
@@ -19,7 +41,7 @@ const Contact = () => {
               </div>
               <div className="c-info-item">
               <img src={Email} alt="" className="c-icon" />
-                contact@angelocode.co
+                contact@angelocode.com
               </div>
               <div className="c-info-item">
               <img src={Address} alt="" className="c-icon" />
@@ -28,10 +50,15 @@ const Contact = () => {
           </div>
           <div className="contact-right">
             <p className="contact-descrip">
-              What can I do for you? Let me know down below!
+              <b>What can I do for you?</b> Let me know down below!
             </p>
-            <form>
-              
+            <form ref={formRef} onSubmit={handleSubmit}>
+              <input id='contactName' type="text" placeholder='Name' name='user_name' required/>
+              <input type="text" placeholder='Subject' name='user_subject' required/>
+              <input type="text" placeholder='Email' name='user_email' required/>
+              <textarea name="message" cols="30" rows="10" placeholder='Write down your message or offer or whatever you want, this actually works!' required></textarea>
+              <button>Submit</button>
+              {send && <b>Thanks you, I'll texting you back!</b>}
             </form>
           </div>
         </div>
